86a76481f3a7f46f50b51c8f3a485a18b672b3e9
root <root@MAMBA.localdomain>
2023-02-26 11:17:11 +0300

    betty

--- main.c
+++ main.c
@@ -11,207 +11,217 @@ int tsh_help(char **args);
 int tsh_exit(char **args);
 
 char *builtin_str[] = {
-  "cd",
-  "help",
-  "exit"
+	"cd",
+	"help",
+ 	"exit"
 };
 
 
 int (*builtin_func[]) (char **) = {
-  &tsh_cd,
-  &tsh_help,
-  &tsh_exit
+	&tsh_cd,
+	&tsh_help,
+	&tsh_exit
 };
 
 /*
-  List of builtin commands, followed by their corresponding functions.
+ * List of builtin commands, followed by
+ *  their corresponding functions.
  */
 
 
-int tsh_num_builtins() {
-  return sizeof(builtin_str) / sizeof(char *);
+int tsh_num_builtins(void)
+{
+       	return (sizeof(builtin_str) / sizeof(char *));
 }
 
 /*
-  Builtin function implementations.
+ * Builtin function implementations.
 */
 int tsh_cd(char **args)
 {
-  if (args[1] == NULL) {
-    fprintf(stderr, "tsh: expected argument to \"cd\"\n");
-  } else {
-    if (chdir(args[1]) != 0) {
-      perror("tsh");
-    }
-  }
-  return 1;
+	if (args[1] == NULL)
+       	{
+		fprintf(stderr, "tsh: expected argument to \"cd\"\n");
+	}
+	else 
+	{
+	if (chdir(args[1]) != 0)
+       	{
+		perror("tsh");
+	}
+	}
+return (1);
 }
 
 int tsh_help(char **args)
 {
-  int i;
-  printf("Type program names and arguments, and hit enter.\n");
-  printf("The following are built in:\n");
+	int i;
+	printf("Type program names and arguments, and hit enter.\n");
+	printf("The following are built in:\n");
 
-  for (i = 0; i < tsh_num_builtins(); i++) {
-    printf("  %s\n", builtin_str[i]);
-  }
+for (i = 0; i < tsh_num_builtins(); i++)
+{
+	printf("  %s\n", builtin_str[i]);
+}
 
-  printf("Use the man command for information on other programs.\n");
-  return 1;
+printf("Use the man command for information on other programs.\n");
+return (1);
 }
 
 int tsh_exit(char **args)
 {
-  return 0;
+return (0);
 }
 
 int tsh_launch(char **args)
 {
-    pid_t pid;
-    int status;
+	pid_t pid;
+	int status;
 
-    pid = fork();
-    if (pid == 0)
-    {
-        if (execvp(args[0], args) == -1)
-        {
-            /* code */
-            perror("tsh");
-        }
-        exit(EXIT_FAILURE);
-    } else if (pid < 0)
-    {
-        /* code */
-        perror("tsh");
-    } else
-    {
-        do {
-        waitpid(pid, &status, WUNTRACED);
-    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
-    
-    return 1;
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+	{
+		perror("tsh");
+	}
+	exit(EXIT_FAILURE);
+	}
+       	else if (pid < 0)
+	{
+		perror("tsh");
+	} else
+	{
+		do {
+ 		waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return (1);
 }
 
 int tsh_execute(char **args)
 {
-  int i;
+	int i;
 
-  if (args[0] == NULL) {
+	if (args[0] == NULL)
+	{
     // An empty command was entered.
-    return 1;
-  }
+	return (1);
+	}
 
-  for (i = 0; i < tsh_num_builtins(); i++) {
-    if (strcmp(args[0], builtin_str[i]) == 0) {
-      return (*builtin_func[i])(args);
-    }
-  }
+for (i = 0; i < tsh_num_builtins(); i++)
+{
+	if (strcmp(args[0], builtin_str[i]) == 0)
+       {
+	return (*builtin_func[i])(args);
+       }
+}
 
-  return tsh_launch(args);
+return tsh_launch(args);
 }
 
 #define BUFSIZE 1024
 char *tsh_read_line(void)
 {
-    int position = 0;
-    int bufsize = BUFSIZE;
-    char c;
-    char *buffer = malloc(sizeof(char) * bufsize);
+	int position = 0;
+	int bufsize = BUFSIZE;
+	char c;
+	char *buffer = malloc(sizeof(char) * bufsize);
 
-    if (!buffer)
-    {
-        printf("tsh: reallocation error\n");
-        exit(EXIT_FAILURE);
-    }
+	if (!buffer)
+	{
+		printf("tsh: reallocation error\n");
+		exit(EXIT_FAILURE);
+	}
 
-    while (1)
-    {
-        c = getchar();
+while (1)
+{
+	c = getchar();
 
-    if (c == EOF) {
-      exit(EXIT_SUCCESS);
-    } else if (c == '\n') {
-      buffer[position] = '\0';
-      return buffer;
-    } else {
-      buffer[position] = c;
-    }
-    position++;
+	if (c == EOF)
+       	{
+	exit(EXIT_SUCCESS);
+	}
+       	else if (c == '\n')
+	{
+	buffer[position] = '\0';
+	return (buffer);
+	} else
+	{
+	buffer[position] = c;
+	}
+	position++;
 
-        if (position >= bufsize)
-        {
-            /* code */
-            bufsize += BUFSIZE;
-            buffer = realloc(buffer,bufsize);
-            if (!buffer)
-            {
-                 printf("tsh: reallocation error\n");
-                 exit(EXIT_FAILURE);
-            }
-        }
-        
-    }
+	if (position >= bufsize)
+	{
+		bufsize += BUFSIZE;
+		buffer = realloc(buffer,bufsize);
+		if (!buffer)
+		{
+			printf("tsh: reallocation error\n");
+			exit(EXIT_FAILURE);
+		}
+	} 
+}
 }
 
 #define TSH_TOK_BUFSIZE 64
 #define TSH_TOK_DELIM " \t\r\n\a"
 char **tsh_split_line(char *line)
 {
-  int bufsize = TSH_TOK_BUFSIZE, position = 0;
-  char **tokens = malloc(bufsize * sizeof(char*));
-  char *token ,**tokens_backup;
+	int bufsize = TSH_TOK_BUFSIZE, position = 0;
+	char **tokens = malloc(bufsize * sizeof(char*));
+	char *token ,**tokens_backup;
 
-  if (!tokens) {
-    fprintf(stderr, "tsh: allocation error\n");
-    exit(EXIT_FAILURE);
-  }
+	if (!tokens) {
+	fprintf(stderr, "tsh: allocation error\n");
+	exit(EXIT_FAILURE);
+	}
 
-  token = strtok(line, TSH_TOK_DELIM);
-  while (token != NULL) {
-    tokens[position] = token;
-    position++;
+	token = strtok(line, TSH_TOK_DELIM);
+	while (token != NULL)
+       	{
+	tokens[position] = token;
+	position++;
 
-    if (position >= bufsize) {
-      bufsize += TSH_TOK_BUFSIZE;
-      tokens_backup = tokens;
-      tokens = realloc(tokens, bufsize * sizeof(char*));
-      if (!tokens) {
-        free(tokens_backup);
-        fprintf(stderr, "tsh: allocation error\n");
-        exit(EXIT_FAILURE);
-      }
-    }
-    token = strtok(NULL, TSH_TOK_DELIM);
- }
-   tokens[position] = NULL;
-  return tokens;
+	if (position >= bufsize)
+	{
+		bufsize += TSH_TOK_BUFSIZE;
+		tokens_backup = tokens;
+		tokens = realloc(tokens, bufsize * sizeof(char*));
+	if (!tokens)
+	{
+	free(tokens_backup);
+	fprintf(stderr, "tsh: allocation error\n");
+	exit(EXIT_FAILURE);
+	}
+	}
+	token = strtok(NULL, TSH_TOK_DELIM);
+	}
+	tokens[position] = NULL;
+	return (tokens);
 }
 
 void tsh_loop(void)
 {
-    char *line;
-    char **args;
-    int status;
+	char *line;
+	char **args;
+	int status;
 
-    do
-    {
-        printf("root@teddy.user # ");
-        line = tsh_read_line();
-        args = tsh_split_line(line);
-        status = tsh_execute(args);
+	do
+	{
+		printf("root@teddy.user # ");
+		line = tsh_read_line();
+		args = tsh_split_line(line);
+ 		status = tsh_execute(args);
 
-        free (line);
-        free (args);
-    } while (status);
-    
+		free (line);
+		free (args);
+	} while (status);
 }
-
-
 
 int main(int argc, char *argv)
 {
-    tsh_loop();
-    return EXIT_SUCCESS;
+	tsh_loop();
+	return (EXIT_SUCCESS);
 }
