0a74aa65a7927e2988e27720b29618664f3d0318
TeddyMuli <tmuli974@gmail.com>
2023-11-07 17:48:40 +0300

    added header file

--- main.h
+++ main.h
@@ -1,31 +1,38 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef MAIN_H
+#define MAIN_H
 
+/* LIBRARIES */
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 
-
-#define TOKEN_DELIMITER "\t\r\n\a\""
-/** main.c */
-void simple_shell(void);
-
-/** simple_shell.c */
-char *read_line(void);
-char **split_line(char *line);
-int execute_cmd(char **args);
-
-/** execute_cmd.c */
-int num_builtin(void);
-int new_process(char **args);
-
+/* MACROS */
+#define TOK_DELIMITER " \t\r\n\a\""
 extern char **environ;
 
-int my_help(char **args);
+/* PROTOTYPES */
+/*---main.c---*/
+void simple_shell(void);
+void non_interactive(void);
+
+/*---shell_interactive---*/
+char *read_line(void);
+char **parse_line(char *line);
+int execute_cmd(char **args);
+
+/*---shell_non_interactive---*/
+char *read_stream(void);
+
+/*---execute_args---*/
+int new_process(char **args);
+int num_builtin(void);
+
+/*---builtin---*/
 int my_cd(char **args);
-int my_env(char **args);
 int my_exit(char **args);
+int my_env(char **args);
+int my_help(char **args);
 
 #endif
